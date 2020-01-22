8dac23d54fc1d6a46bc964083531c1757e493795
Josh P. Sawyer <josh.p.sawyer@gmail.com>
2020-01-22 16:20:41 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,22 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="extarc", # Replace with your own username
+    name="extarc-joshpsawyer", # Replace with your own username
     version="0.0.1",
     author="Josh P. Sawyer",
     author_email="josh.p.sawyer@gmail.com",
     description="A collection of functions for extending ESRI's arcpy module. Mostly utility or helper functions.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/joshpsawyer/arcutils",
+    url="https://github.com/joshpsawyer/extarc",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         "License :: OSI Approved :: GNU GPLv3",
-        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+#   currently, no explicitly set python version requirement            
+#    python_requires='>=3.6',
 )
